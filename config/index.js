8aef9336cc0da17f825f8e62cb4b1047cf22fdc0
Leo Borquez <leoborquezg@protonmail.com>
2021-09-08 10:48:34 -0400

    url api env vars

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 export const clientID = process.env.NEXT_PUBLIC_CLIENT_ID
+export const apiURL = process.env.NEXT_PUBLIC_API_URL
+export const apiKey = process.env.NEXT_PUBLIC_API_KEY
 
-console.log("asdasd " + clientID)
 
 const dev = process.env.NODE_ENV !== 'production'
 export const server = dev ? 'http://localhost:3000' : 'https://sitedomain.com'
--- pages/api/instances/index.js
+++ pages/api/instances/index.js
@@ -1,24 +1,29 @@
+ import { apiURL, apiKey } from '../../../config'
+
  async function handler(req, res) {
  
-    const response = await fetch('https://cloudbilling.googleapis.com/v1/services/6F81-5844-456A/skus?key=AIzaSyDVWa131rNBwaTJ29-Of1YfKBHAHwyiW18')
+    //const response = await fetch('https://cloudbilling.googleapis.com/v1/services/6F81-5844-456A/skus?key=AIzaSyDVWa131rNBwaTJ29-Of1YfKBHAHwyiW18&pageToken=AD4oVHCXlSQGTGWJhxOZyHrkvRj-HjuCkyMOEvT_KefZBhP8NX7vD7CFRNcHoQwcu-4dFRZyfPoh4Q8QnnV-T0qTvVntOjrpl8NxXYneOpPqsdE4Rpc4c4k=')
+    const response = await fetch(`${apiURL}/6F81-5844-456A/skus?key=${apiKey}`)
+    console.log(`${apiURL}/6F81-5844-456A/skus?key=${apiKey}`)
     const data = await response.json()
+    const token = data.nextPageToken
     const filter = [] 
-    /*
-    data.skus.forEach(obj => {
-        if (obj.serviceRegions == "europe-west6"){
-            console.log(obj)
-        }
-    });
-    */
-
+    
+    // get the filtered instance
     data.skus.forEach(obj => {
         if(obj.category.resourceGroup == "N1Standard"){
-            filter.push(obj)
+            obj.geoTaxonomy.regions.forEach(region => {
+                if(region == "us-central1"){
+                    filter.push(obj)
+                }
+            })
+            console.log(obj.geoTaxonomy.regions)
+            //filter.push(obj)
         }
     })
-
+    
     res.statusCode = 200
-    return res.json(filter)
+    return res.json(data)
 }
 
 export default handler;
